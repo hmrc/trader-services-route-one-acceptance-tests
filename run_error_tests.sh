c145532a111c41cfce0686fddcee18277079b442
Tasmine10ten <55190161+Tasmine10ten@users.noreply.github.com>
2021-04-13 09:28:20 +0100

    add error runner

--- run_error_tests.sh
+++ run_error_tests.sh
@@ -9,5 +9,5 @@ elif [ "$BROWSER" = "firefox" ]; then
     DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
 fi
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner"
+sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.test.ui.cucumber.runner.RunnerErrors"
 
