b823cf3339ab35c2b44985f536d1c6cacd1fb1c8
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2023-06-14 10:39:56 +0100

    Revert "DDCYLS-1818: update webdriver-factory to version 0.44.0"
    This reverts commit 2569899c17594db510ffc3b68e8f22a4e6b66fd6.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.44.0"   % Test,
+    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.41.0"   % Test,
     "org.scalatest"          %% "scalatest"               % "3.2.13"   % Test,
     "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0" % Test,
     "io.cucumber"            %% "cucumber-scala"          % "8.6.0"    % Test,
