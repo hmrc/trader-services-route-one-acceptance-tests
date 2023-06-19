7b91204913f9a3a7c7f2aaf5ca9ec94005a0aa8d
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2023-06-19 15:56:52 +0100

    DDCYLS-2752: update webdriver-factory to version 0.44.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.41.0"   % Test,
-    "org.scalatest"          %% "scalatest"               % "3.2.13"   % Test,
+    "uk.gov.hmrc"            %% "webdriver-factory"       % "0.44.0"   % Test,
+    "org.scalatest"          %% "scalatest"               % "3.2.15"   % Test,
     "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0" % Test,
-    "io.cucumber"            %% "cucumber-scala"          % "8.6.0"    % Test,
-    "io.cucumber"             % "cucumber-junit"          % "7.8.0"    % Test,
+    "io.cucumber"            %% "cucumber-scala"          % "8.14.1"    % Test,
+    "io.cucumber"             % "cucumber-junit"          % "7.11.1"    % Test,
     "junit"                   % "junit"                   % "4.13.2"   % Test,
     "com.novocode"            % "junit-interface"         % "0.11"     % Test,
     "com.typesafe"            % "config"                  % "1.4.2"    % Test,
-    "org.seleniumhq.selenium" % "selenium-chrome-driver"  % "4.4.0"    % Test,
-    "org.seleniumhq.selenium" % "selenium-support"        % "4.4.0"    % Test,
-    "org.seleniumhq.selenium" % "selenium-firefox-driver" % "4.4.0"    % Test,
-    "org.jsoup"               % "jsoup"                   % "1.15.3"   % Test
+    "org.seleniumhq.selenium" % "selenium-chrome-driver"  % "4.8.1"    % Test,
+    "org.seleniumhq.selenium" % "selenium-support"        % "4.8.1"    % Test,
+    "org.seleniumhq.selenium" % "selenium-firefox-driver" % "4.8.1"    % Test,
+    "org.jsoup"               % "jsoup"                   % "1.15.4"   % Test
   )
 }
