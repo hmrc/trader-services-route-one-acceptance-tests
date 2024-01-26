f872e0525b9f58a001f214312a7bbbecd51a9328
liam.henderson <35490828+liamhenderson92@users.noreply.github.com>
2024-01-26 14:25:20 +0000

    DDCYLS-4237: Upgrade selenium and dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "webdriver-factory"       % "0.46.0"   % Test,
-    "org.scalatest"          %% "scalatest"               % "3.2.15"   % Test,
-    "org.scalatestplus"      %% "selenium-4-2"            % "3.2.13.0" % Test,
+    "org.scalatest"          %% "scalatest"               % "3.2.17"   % Test,
+    "org.scalatestplus"      %% "selenium-4-12"           % "3.2.17.0" % Test,
     "io.cucumber"            %% "cucumber-scala"          % "8.14.1"   % Test,
     "io.cucumber"             % "cucumber-junit"          % "7.11.1"   % Test,
     "junit"                   % "junit"                   % "4.13.2"   % Test,
