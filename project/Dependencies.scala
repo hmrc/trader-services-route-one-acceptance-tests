6cb0f417b0aeef0ddce00c3fb647193c90217f4c
liam.henderson <35490828+liamhenderson92@users.noreply.github.com>
2025-10-21 16:57:44 +0100

    upgrade ui-test runner

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "ui-test-runner"          % "0.45.0",
+    "uk.gov.hmrc"            %% "ui-test-runner"          % "0.50.0",
     "org.scalatestplus"      %% "selenium-4-12"           % "3.2.17.0",
-    "io.cucumber"            %% "cucumber-scala"          % "8.21.0",
-    "io.cucumber"             % "cucumber-junit"          % "7.18.0",
+    "io.cucumber"            %% "cucumber-scala"          % "8.35.0",
+    "io.cucumber"             % "cucumber-junit"          % "7.30.0",
     "com.novocode"            % "junit-interface"         % "0.11"
   ).map(_ % Test)
 }
